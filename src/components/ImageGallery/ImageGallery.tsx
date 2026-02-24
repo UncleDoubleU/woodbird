1b6e5c10c322b171f0b0af9a9bb3257d4fb96373
UncleDoubleU <alexmozagba@gmail.com>
2026-02-24 18:21:53 +0100

    basic image gallery layout implement - for desktops

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -5,64 +5,99 @@ function ImageGallery() {
           {
                id: 1,
                imgSrc: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg",
-               imgAlt: "Contemporary Scandinavian fashion editorial"
+               imgAlt: "Contemporary Scandinavian fashion editorial",
+               colSpan: "1",
+               imgRow: 1,
+               rowSpan: 2
           },
           {
                id: 2,
                imgSrc: "https://images.pexels.com/photos/2983463/pexels-photo-2983463.jpeg",
-               imgAlt: "Editorial fashion model portrait"
+               imgAlt: "Editorial fashion model portrait",
+               colSpan: "2",
+               imgRow: 1,
+               rowSpan: 3
           },
           {
                id: 3,
                imgSrc: "https://images.pexels.com/photos/2983461/pexels-photo-2983461.jpeg",
-               imgAlt: "High fashion editorial runway look"
+               imgAlt: "High fashion editorial runway look",
+               colSpan: "1",
+               imgRow: 1,
+               rowSpan: 1
           },
           {
                id: 4,
                imgSrc: "https://images.pexels.com/photos/2983467/pexels-photo-2983467.jpeg",
-               imgAlt: "Urban editorial fashion photography"
+               imgAlt: "Urban editorial fashion photography",
+               colSpan: "4",
+               imgRow: 1,
+               rowSpan: 1
           },
           {
                id: 5,
                imgSrc: "https://cdn.pixabay.com/photo/2025/03/12/00/00/fashion-editorial-9464921_1280.jpg",
-               imgAlt: "Fashion editorial photography modern style"
+               imgAlt: "Fashion editorial photography modern style",
+               colSpan: "1",
+               imgRow: 1,
+               rowSpan: 1
           },
           {
                id: 6,
                imgSrc: "https://images.pexels.com/photos/2983470/pexels-photo-2983470.jpeg",
-               imgAlt: "Japanese inspired editorial fashion"
+               imgAlt: "Japanese inspired editorial fashion",
+               colSpan: "2",
+               imgRow: 1,
+               rowSpan: 3
           },
           {
                id: 7,
                imgSrc: "https://images.pexels.com/photos/2983462/pexels-photo-2983462.jpeg",
-               imgAlt: "Contemporary fashion editorial portrait"
+               imgAlt: "Contemporary fashion editorial portrait",
+               colSpan: "1",
+               imgRow: 1,
+               rowSpan: 2
           },
           {
                id: 8,
                imgSrc: "https://images.pexels.com/photos/2983465/pexels-photo-2983465.jpeg",
-               imgAlt: "Editorial fashion artistic pose"
+               imgAlt: "Editorial fashion artistic pose",
+               colSpan: "1",
+               imgRow: 2,
+               rowSpan: 3
           },
           {
                id: 9,
                imgSrc: "https://images.pexels.com/photos/2983472/pexels-photo-2983472.jpeg",
-               imgAlt: "Editorial fashion scene outdoors"
+               imgAlt: "Editorial fashion scene outdoors",
+               colSpan: "2",
+               imgRow: 2,
+               rowSpan: 4
           },
           {
                id: 10,
                imgSrc: "https://images.pexels.com/photos/2983469/pexels-photo-2983469.jpeg",
-               imgAlt: "Studio fashion editorial look"
+               imgAlt: "Studio fashion editorial look",
+               colSpan: "1",
+               imgRow: 2,
+               rowSpan: 3
           }
      ];
 
      return (
-          <article className="grid grid-cols-[12] grid-rows-2 w-full bg-red-400">
+          <article className="grid grid-cols-[1.5fr_2.25fr_1fr_4fr_1fr_2.25fr_1.5fr] grid-rows-5 gap-3 w-full bg-red-400 h-[35rem] justify-center">
                {imageList.length === 10 ? imageList.map(pic =>
-                    <div>
+                    <div style={{
+
+                         gridRow: `span ${pic.rowSpan}`
+                    }}>
                          <img
                               key={pic.id}
                               src={pic.imgSrc}
                               alt={pic.imgAlt}
+                              className="object-cover bg-neutral-800 w-full h-full"
                               width={300}
+                              height={600}
 
                          />
                     </div>
--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -3,10 +3,10 @@ import { HeartIcon } from '@heroicons/react/16/solid'
 
 function MainContent() {
      return (
-          <main className='w-full pt-5 px-6'>
+          <main className='w-full pt-5 px-6 text-neutral-800'>
                <article className='flex h-screen justify-between items-center'>
                     <HeartIcon className='size-7 fill-neutral-800' />
-                    <h1 className='font-bold text-center text-[2.75rem]/[2.5rem] md:text-[7rem]/[5.5rem] xl:text-[12rem]/[10rem] scale-y-175'>
+                    <h1 className='text-neutral-800 font-bold text-center text-[2.75rem]/[2.5rem] md:text-[7rem]/[5.5rem] xl:text-[12rem]/[10rem] scale-y-175'>
                          BRAND<br />
                          MADE WITH<br />
                          HEART
